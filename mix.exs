5577f08198931803761b2d68c3bb6d37f95b7c3b
Pedro Martins <pedroma@BCNPEDMARTMAC.local>
2019-10-09 15:08:08 +0200

    updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FootbalEngine.MixProject do
   def project do
     [
       app: :footbal_engine,
-      version: "1.0.0",
+      version: "2.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
